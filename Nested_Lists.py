a58aa6d5e827913f42336bb4a3d58263b0ecea8c
gmguru16 <36320275+gmguru16@users.noreply.github.com>
2019-06-18 16:52:43 +0530

    Create Nested_Lists.py

---
+++ Nested_Lists.py
@@ -0,0 +1,15 @@
+mark=[]
+score=[]
+
+for _ in range(int(input())):
+        a = input()
+        b = float(input())
+        mark+=[[a,b]]
+        score+=Nested Lists
+
+[b]
+b=sorted(list(set(score)))[1] 
+
+for a,c in sorted(mark):
+        if c==b:
+            print(a)
